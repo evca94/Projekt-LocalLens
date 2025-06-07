d883724f8e1a212077e98c52423612e36a3f0c68
Lenka Koubková <lvseteckova@gmail.com>
2025-06-07 11:10:12 +0200

    Komponenty PageTitle, Uvod

---
+++ src/Components/PageTitle.jsx
@@ -0,0 +1,9 @@
+export const PageTitle = () => {
+  return (
+    <div className="app-header">
+      <header>
+        <h1 className="app-title">Local Lens</h1>
+      </header>
+      </div>
+  )
+  }
---
+++ src/Components/Uvod.jsx
@@ -0,0 +1,10 @@
+export const Uvod = () => {
+  return (
+    <div className="app-header">
+      <header>
+        <h2 className="app-title">Co tu najdeš? 
+13 krajských měst,každé město s připraveným plánem, jaké nejlepší podniky a místa navštívit za jeden víkend. </h2>
+      </header>
+      </div>
+  )
+  }
