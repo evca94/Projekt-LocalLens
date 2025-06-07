cde421adc1d43d4ac1ee8b3ea1fa3c3fb7fe1138
Lenka Koubková <lvseteckova@gmail.com>
2025-06-07 18:19:20 +0200

    komponenty

---
+++ src/Components/Category/Category.jsx
@@ -0,0 +1,26 @@
+import { BodProgramu } from "../BodProgramu/BodProgramu"
+import { NazevActivity } from "../NazevActivity/NazevActivity"
+
+export const Category = ({name, items} ) => {
+if (items || items.length === 0) {
+  return null
+}
+
+
+ return (
+    <div>
+    
+  <NazevActivity 
+  activity={name} />
+  {items.map((item) => {
+    
+    return ( 
+    <BodProgramu
+     key={name}
+     program={item} />
+  )
+  })}
+  </div>
+ )
+
+}
--- src/pages/ItinerarPage/ItinerarPage.jsx
+++ src/pages/ItinerarPage/ItinerarPage.jsx
@@ -3,15 +3,34 @@ import { Navigace } from '../../Components/Navigace/Navigace';
 import { DenTitle } from '../../Components/DenTitle/DenTitle';
 import { NazevActivity } from '../../Components/NazevActivity/NazevActivity';
 import { BodProgramu } from '../../Components/BodProgramu/BodProgramu';
+import { useParams } from 'react-router-dom';
+import  itinerary from '../../../data/itinerary.json'
+import { Category } from '../../Components/Category/Category';
 
 export const ItinerarPage = () => {
+const {den} = useParams();
+const filteredItinerar = itinerary.filter((item => den === item.day))
+const categories = ["Snídaně","Dopolední aktivity", "Oběd", "Odpolední aktivity", "Sportovní aktivity", "Večeře", "Večerní program" ]
+
+
+
   return (
     <>
    <PageTitle />
    <Navigace />
-    <DenTitle  />
-    <NazevActivity />
-    <BodProgramu />
+    <DenTitle 
+    dayTitle={den} />
+    {categories.map((category) => {
+      const filteredItems = filteredItinerar.filter((item ) => category === item.category)
+    
+      return (
+        <Category 
+        key={category}
+        name={category} items={filteredItems} />
+      )
+    }
+    )
+    }
 
    </>
   );
