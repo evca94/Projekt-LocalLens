87ead78e933e452ae3cfe8e94a418514e3f2ac35
Lenka Koubková <lvseteckova@gmail.com>
2025-06-08 11:28:43 +0200

    úprava komponenty Carousel

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -3,15 +3,7 @@ import cities from '../../../data/cities.json'
 
 export const Carousel = ({image} ) => {
   return (
-    <>
-    <Link to="img src={city.image[0]}"alt={city.name} />
-    <h1>{city.name}</h1></ Link>
- <div class="slide-images">
-       <div class="slider-img">
-         <img src="images/CeskeBudejovice.jpg" alt="CeskeBudejovice" />
-         <h1>České Budějovice</h1>
-       </div>
-
+ 
   )
   
-  
+  }
