85a057093ddf54c3f42ae432eef401ae56679447
Lenka Koubková <lvseteckova@gmail.com>
2025-06-08 11:03:35 +0200

    komponenta Carousel

---
+++ src/Components/Carousel/Carousel.jsx
@@ -0,0 +1,14 @@
+import cities from '../../../data/cities.json'
+
+
+export const Carousel = ({image} ) => {
+  return (
+    <>
+    <Link to="img src={city.image[0]}"alt={city.name} />
+    <h1>{city.name}</h1></ Link>
+
+    
+    </>
+  )
+  
+  
