326b797133169b6e8d4542a57d8925e08001c7cf
Lenka Koubková <lvseteckova@gmail.com>
2025-06-08 12:36:12 +0200

    tlačítka v Carousel.jsx

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,9 +1,33 @@
+import { useState } from 'react'
 import cities from '../../../data/cities.json'
+import './Carousel.css'
 
 
-export const Carousel = ({image} ) => {
-  return (
- 
-  )
-  
+export const Carousel = () => {
+  const [activeIndex, setActiveIndex] = useState(1)
+
+  const handleClickNext = () => {
+    setActiveIndex(activeIndex + 1)
   }
+  const handleClickPrev = () =>{
+    setActiveIndex(activeIndex - 1)
+  }
+  
+  return(
+    <div className='slider'>
+    {cities.map((city) => {
+    return (
+      <Slide
+      key={city.id}
+      slide={city} />
+     )
+  })}
+  <button onClick={handleClickNext} id="next"> {">"}</button>
+ <button onClick={handleClickPrev} id="prev">{"<"}</button>
+
+  </div>
+  )
+
+  }
+
+  
