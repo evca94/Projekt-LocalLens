dea2c27dfbaed74befe4fb7c43c09514b0900b34
Lenka Koubková <lvseteckova@gmail.com>
2025-06-08 16:40:20 +0200

    úprava textu

--- src/Components/Slide/Slide.jsx
+++ src/Components/Slide/Slide.jsx
@@ -1,34 +1,12 @@
-import "./slide.css";
-const styleActive = {
-  transform: 'none',
-  zIndex: 1,
-  filter: 'none',
-  opacity: 1,
-};
-
-export const Slide = ({ slide, slideIndex, activeIndex }) => {
-  let style = {};
-  if (slideIndex === activeIndex) {
-    style = styleActive;
-  } else {
-    const absDiff = Math.abs(slideIndex - activeIndex);
-    style = {
-      zIndex: -absDiff,
-      filter: "blur(5px)",
-      opacity: absDiff > 2 ? 0 : 0.6,
-    };
-
-    if (slideIndex > activeIndex) {
-      style.transform = `translateX(${120*absDiff}px) scale(${1 - 0.2*absDiff}) perspective(16px) rotateY(-1deg)`;
-  } else {
-    style.transform = `translateX(${-120*absDiff}px) scale(${1 - 0.2*absDiff}) perspective(16px) rotateY(-1deg)`
-  }
-}
-
+import './slide.css'
+export const Slide = ({slide}) => {
+  
   return (
-    <div className="item" style={style}>
-      <h1>{slide.name} </h1>
-      <div> {slide.description}</div>
-    </div>
-  );
+    <>
+<div class="item">
+   <h1>{slide.name} </h1>
+   <div> {slide.description}</div>
+ </div>
+</>
+  )
 };
