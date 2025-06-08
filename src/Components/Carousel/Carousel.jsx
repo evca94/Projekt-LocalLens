3f5ff8339a20fa1b3df289b2805e4e5753007665
Evca94 <evca.94@seznam.cz>
2025-06-08 11:12:46 +0200

    přidání class, obrázek města na hlavní stranu

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -6,9 +6,12 @@ export const Carousel = ({image} ) => {
     <>
     <Link to="img src={city.image[0]}"alt={city.name} />
     <h1>{city.name}</h1></ Link>
+ <div class="slide-images">
+       <div class="slider-img">
+         <img src="images/CeskeBudejovice.jpg" alt="CeskeBudejovice" />
+         <h1>České Budějovice</h1>
+       </div>
 
-    
-    </>
   )
   
   
