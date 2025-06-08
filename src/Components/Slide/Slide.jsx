625df4c0f355f64d296d97f775e38b5cb356b446
Lenka Koubková <lvseteckova@gmail.com>
2025-06-08 18:21:52 +0200

    úprava Slide.jsx

--- src/Components/Slide/Slide.jsx
+++ src/Components/Slide/Slide.jsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
-import "./Slide.css";
+import { Link } from 'react-router-dom';
+import './Slide.css';
 const styleActive = {
   transform: 'none',
   zIndex: 1,
@@ -15,31 +15,35 @@ export const Slide = ({ slide, slideIndex, activeIndex }) => {
     const absDiff = Math.abs(slideIndex - activeIndex);
     style = {
       zIndex: -absDiff,
-      filter: "blur(5px)",
+      filter: 'blur(5px)',
       opacity: absDiff > 2 ? 0 : 0.6,
     };
 
     if (slideIndex > activeIndex) {
-      style.transform = `translateX(${120*absDiff}px) scale(${1 - 0.2*absDiff}) perspective(16px) rotateY(-1deg)`;
-  } else {
-    style.transform = `translateX(${-120*absDiff}px) scale(${1 - 0.2*absDiff}) perspective(16px) rotateY(-1deg)`
+      style.transform = `translateX(${120 * absDiff}px) scale(${
+        1 - 0.2 * absDiff
+      }) perspective(16px) rotateY(-1deg)`;
+    } else {
+      style.transform = `translateX(${-120 * absDiff}px) scale(${
+        1 - 0.2 * absDiff
+      }) perspective(16px) rotateY(-1deg)`;
+    }
   }
-}
-if (slide.name==="České Budějovice") {
-  return <Link to="/mesto" >
-    <div className="item" style={style}>
-   <h1>{slide.name} </h1>
-   <div> {slide.description}</div>
- </div>
-  </Link>
-}
+  if (slide.name === 'České Budějovice') {
+    return (
+      <Link to="/mesto">
+        <div className="item" style={{...style, backgroundImage: `url(${slide.image})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'cover'}}>
+          <h1>{slide.name} </h1>
+          {/* <div> {slide.description}</div> */}
+        </div>
+      </Link>
+    );
+  }
 
   return (
-    
-<div className="item" style={style}>
-   <h1>{slide.name} </h1>
-   <div> {slide.description}</div>
- </div>
-
-  )
+    <div className="item" style={{...style, backgroundImage: `url(${slide.image})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'cover'}}>
+      <h1>{slide.name} </h1>
+      {/* <div> {slide.description}</div> */}
+    </div>
+  );
 };
