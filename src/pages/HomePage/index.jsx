4bfc975cb03abd8667e01838ceaf23ad10035995
Evca94 <evca.94@seznam.cz>
2025-06-08 16:10:34 +0200

    změny ve stylování

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,14 +1,18 @@
 import { PageTitle } from '../../Components/PageTitle/PageTitle';
 import { Uvod } from '../../Components/Uvod/Uvod';
 import { Carousel} from '../../Components/Carousel/Carousel';
+import './style.css';
 
 export const HomePage = () => {
   return (
-    <>
+
+     <div className="home-page-content">
+      <div></div>
    <PageTitle />
    <Carousel />
-   <Uvod />
-   </>
+    <Uvod />
+   </div>
+  
   );
 };
 
