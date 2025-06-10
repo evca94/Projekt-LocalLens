085e58ba797fd119fec1b35e6b3763de21cab6ff
Evca94 <evca.94@seznam.cz>
2025-06-10 16:01:14 +0200

    opravení hrubky v textu

--- src/Components/Uvod/Uvod.jsx
+++ src/Components/Uvod/Uvod.jsx
@@ -4,7 +4,7 @@ export const Uvod = () => {
     <div className="app-header">
       <header>
         <h2 className="city-description">Co tu najdeš? 
-13 krajských měst,každé město s připraveným plánem, jaké nejlepší podniky a místa navštívit za jeden víkend. </h2>
+13 krajských měst, každé město s připraveným plánem, jaké nejlepší podniky a místa navštívit za jeden víkend. </h2>
       </header>
       </div>
   )
